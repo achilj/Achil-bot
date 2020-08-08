e7f8597378237a435f10822182a4d8f2f5139b28
achilj <39186535+achilj@users.noreply.github.com>
2020-08-08 02:33:09 +0200

    ticket2.0

--- comands/ticket.js
+++ comands/ticket.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
  
     // ID van de categorie van de tickets.
-    const categoryId = "644213232906534915";
+    const categoryId = "741452078144290866";
  
     // Verkrijg Gebruikersnaam
     var userName = message.author.username;
