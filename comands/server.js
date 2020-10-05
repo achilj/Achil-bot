cdedb3029e66b4b4e3d82330a483d15fc0d0edff
achilj <39186535+achilj@users.noreply.github.com>
2020-10-05 20:02:29 +0200

    Update server.js

--- comands/server.js
+++ comands/server.js
@@ -9,7 +9,9 @@ module.exports.run = async (bot, message, args) => {
         .setDescription("'2020-2021'")
         .setColor("#FF0000")
         .setThumbnail("https://cdn.discordapp.com/icons/302453258100670465/ccda5964ec02251300c46fa8b3d9f6ac.png")
-        .addField("IP: **dewijnpers.serv.nu**");
+        .addFields(
+            {name: "IP: **dewijnpers.serv.nu**"}
+        );
 
     return message.channel.send(serverEmbed);
 
