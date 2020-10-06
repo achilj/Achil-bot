c9ea443f2c95ce091214c718ae0ee6283cfcdcba
achilj <39186535+achilj@users.noreply.github.com>
2020-10-06 18:52:00 +0200

    asf

--- comands/server.js
+++ comands/server.js
@@ -3,14 +3,14 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     var icon = message.guild.iconURL;
+    
+    message.delete();
 
     var serverEmbed = new discord.RichEmbed()
         .setTitle("De Wijnpers survival:")
-        .setDescription("'2020-2021'")
+        .setDescription("IP: **dewijnpers.serv.nu**")
         .setColor("#FF0000")
         .setThumbnail("https://cdn.discordapp.com/icons/302453258100670465/ccda5964ec02251300c46fa8b3d9f6ac.png")
-        .addField("IP: **dewijnpers.serv.nu**")
-        .addField("Gemaakt op:", bot.user.createdAt)
         .setFooter("Made By DE WIJNPERS");
 
     return message.channel.send(serverEmbed);
