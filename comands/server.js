c806d33243562138cfba3883f8c4b76d3863b5f5
achilj <39186535+achilj@users.noreply.github.com>
2020-10-07 18:47:26 +0200

    Update server.js

--- comands/server.js
+++ comands/server.js
@@ -1,22 +1,21 @@
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+
+    var icon = message.guild.iconURL;
     
     message.delete();
 
-    var botEmbed = new discord.MessageEmbed()
-    .setTitle("**__DE FIVEM SERVER?__**")
-    .setDescription("*Hier vind je informatie over de wijnpers server!*")
-    .setColor("#FF0000")
-    .addFields(
-        {name: "*__Het IP ADRES:__*", value: "**dewijnpers.serv.nu**"},
-        {name: "*__De discord server:__*", value: "https://invite.gg/dewijnpers"}
-    )
-    .setThumbnail("https://cdn.discordapp.com/icons/302453258100670465/ccda5964ec02251300c46fa8b3d9f6ac.png")
-    .setFooter("Made By WIJNPERS", "https://cdn.discordapp.com/icons/302453258100670465/ccda5964ec02251300c46fa8b3d9f6ac.png")
-    .setTimestamp("Now")
+    var serverEmbed = new discord.RichEmbed()
+        .setTitle("De Wijnpers survival:")
+        .setDescription("2020-2021")
+        .setColor("#FF0000")
+        .addField("Gemaakt op:", "ddd")
+        .setThumbnail("https://cdn.discordapp.com/icons/302453258100670465/ccda5964ec02251300c46fa8b3d9f6ac.png")
+        .setFooter("Made By DE WIJNPERS");
 
-    return message.channel.send(botEmbed);
+    return message.channel.send(serverEmbed);
+
 }
 
 module.exports.help = {
