1ef4462a0fddb90453ac4b639ff688cf3d1f9bbb
achilj <39186535+achilj@users.noreply.github.com>
2020-10-17 12:46:21 +0200

    Update mcstatus.js

--- comands/mcstatus.js
+++ comands/mcstatus.js
@@ -1,1 +1,22 @@
-hellow world
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    ping('dewijnpers.serv.nu', 25565, (error, reponse) =>{
+    
+      if(error) throw error
+      const Embed = new RichEmbed()
+      .setTitle('Server Status')
+      .addField('Server IP', reponse.host)
+      .addField('Server Version', reponse.version)
+      .addField('Online Players', reponse.onlinePlayers)
+      .addField('Max Players', reponse.maxPlayers)
+                
+      
+    return message.channel.send(Embed);
+
+}
+
+module.exports.help = {
+    name: "mcstatus"
+}
